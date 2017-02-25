f24a9cefcc2603119a96b98723ee8d6c4d603598
Daniel Andersson <danielandersson@Daniels-MacBook-Air.local>
2017-02-25 22:37:38 +0100

    oh yeah

--- parseLopp.m
+++ parseLopp.m
@@ -1,11 +1,14 @@
 
 source = urlread('https://www.travsport.se/sresultat?kommando=tevlingsdagVisa&tevdagId=560015&loppId=1060056&source=S#lopp');
-
+%source = urlread('https://www.travsport.se/sresultat?kommando=tevlingsdagVisa&tevdagId=560015&loppId=1060100&source=S#lopp');
 
 
 
 %%
 clc
+%%%%%%%%%%%%%%%%%%%
+%    GET DATA     %
+%%%%%%%%%%%%%%%%%%%
 
 %%% Infotext %%%
 
@@ -39,7 +42,7 @@ placeringTabell_end   = placeringTabell_start + placeringTabell_end(1) + 6;
 testFirst_plac_start = source(placeringTabell_start);
 testFirst_plac_end   = source(placeringTabell_end);
 
-table_placering = source(placeringTabell_start:placeringTabell_end);
+placeringsTabell = source(placeringTabell_start:placeringTabell_end);
 
 %%
 clc
@@ -70,7 +73,67 @@ infoTabell_3 = source(infoTabell_start_3:infoTabell_end_3);
 
 infoTabell = strcat(infoTabell_1,strcat(infoTabell_2,infoTabell_3));
 %%
+clc
+%%%%%%%%%%%%%%%%%%%
+%    WRITE DATA   %
+%%%%%%%%%%%%%%%%%%%
+
+tr_long   = '<tr class="header">';
+bstr      = '</tr>';
+
+header_start = strfind(placeringsTabell,tr_long);
+header_end   = strfind(placeringsTabell(header_start:end),bstr);
+header_end   = header_start + header_end(1) + 3;
+
+testHeader_start = placeringsTabell(header_start);
+testHeader_end   = placeringsTabell(header_end);
+
+part_header = placeringsTabell(header_start:header_end);
 
 
+th      = '<th>';
+th_long = '<th class="center">';
+bsth    = '</th>';
 
+nr_headers = length(strfind(part_header, '<th'));
+
+idx_from     = strfind(part_header,th) + length(th);
+idx_to       = strfind(part_header,bsth) - 1;
+alt_idx_from = strfind(part_header,th_long) + length(th_long);
+
+idx_from = sort([idx_from alt_idx_from]);
+
+headers = cell(nr_headers,1);
+for i = 1:nr_headers
+    headers(i) = {part_header(idx_from(i):idx_to(i))};
+end
+headers;
+
+even = '<tr class=""';
+odd  = '<tr class="odd"';
+
+nr_even  = length(strfind(placeringsTabell,even));
+nr_odd   = length(strfind(placeringsTabell,odd));
+nrOfRows = nr_even + nr_odd;
+
+part_even = strfind(placeringsTabell,even);
+part_odd  = strfind(placeringsTabell,odd);
+
+for i = 1:nrOfRows/2
+    row = placeringsTabell(part_even(i):part_odd(i));
+    
+    if eq(i,nrOfRows/2)
+        end_idx = strfind(placeringsTabell(part_odd(i):end),bstr);
+        end_idx = part_odd(i) + end_idx(1) + 4;
+        row = placeringsTabell(part_odd(i):end_idx);
+    end
+    
+    col = strfind(row,'<td');
+    
+    for j = 1:length(col)
+        cellVal = row(col(j):col(j+1)) 
+    end
+    
+end
+row;
 
