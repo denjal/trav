4339ff4bc7a2501f046f4452a50279292f62dfb3
MacBook Pro <macbookpro@MacBooks-MacBook-Pro-3.local>
2017-03-04 17:30:34 +0100

    update

--- Trav.m
+++
@@ -1,4 +0,0 @@
-str = urlread('https://www.travsport.se/sresultat?kommando=tevlingsdagVisa&tevdagId=560015&loppId=1060056&source=S#lopp');
-str2 = webread('https://www.travsport.se/sresultat?kommando=tevlingsdagVisa&tevdagId=560015&loppId=1060056&source=S#lopp');
-
-
